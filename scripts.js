49e3af8f722f3f8d58e32932434276943891a7e6
sangeeth <sangeethsubramoniam@gmail.com>
2025-02-24 23:11:57 +0900

    OTA Website first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Simple script to handle the contact form submission
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! We will get back to you shortly.');
+});
